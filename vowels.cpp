c1e4b41a0a14b940f4a4c1ced64bb4fe23297bef
VictoriaDudu <dyudyunova@inbox.ru>
2024-03-23 21:51:31 +0700

    stroke.cpp
    ввод строки

---
+++ vowels.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cout << "¬ведите строку из символов (латинские буквы):" << endl;
+    getline(cin, str);
+}
