42e500da63c82a19acfc50c8dda326c762473338
VictoriaDudu <dyudyunova@inbox.ru>
2024-03-23 22:06:40 +0700

    vowels.cpp
    подсчет количества гласных букв в строке

--- vowels.cpp
+++ vowels.cpp
@@ -5,7 +5,23 @@ using namespace std;
 
 int main()
 {
+    const int N = 12;
+    char vowel[N] = { 'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y' };
     string str;
     cout << "Введите строку из символов (латинские буквы):" << endl;
     getline(cin, str);
+    int kolichestvo = 0;
+    for (int i = 0; str[i]; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (str[i] == vowel[j])
+                kolichestvo++;
+        }
+    }
+    if (kolichestvo)
+        cout << "Количество гласных символов: " << kolichestvo << endl;
+    else
+        cout << "Гласных символов не найдено!" << endl;
+    return 0;
 }
